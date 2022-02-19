500eb3b356b0fd62874f720020b287c54d4fbb1d
vinu <85886469+vinu94-s@users.noreply.github.com>
2022-02-19 19:52:40 +0530

    Add files via upload

---
+++ Frontend/src/components/utilities_/myloader.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Classes from './style1.module.css';
+
+const Loader = (props) => {
+
+   let cl = [Classes.loader,'mx-auto']
+    return props.center?
+    (
+     <>
+      <div style={{position:'absolute' ,top:'45%',right:'45%'}} className={cl.join(' ')}>
+            
+            </div>
+
+     </>
+    ):(
+        <div className={cl.join(' ')}>
+            
+        </div>
+    )
+}
+
+export default Loader;
